9ecb39b7ffa8d950c146911ec1c256b3108e57f2
agoladamaris <agoladamaris@gmail.com>
2022-11-07 01:58:45 -0800

    third commit

---
+++ grading.js
@@ -0,0 +1,27 @@
+function grading(grade){
+    if (grade >= 90) {
+        console.log(`The grade is A`);
+    }
+    else if(grade >=80){
+        console.log(`The grade is A-`);
+    } 
+        else if(grade >=70){
+            console.log(`The grade is B`); 
+        }
+            else if(grade >=60){
+                console.log(`The grade is B-`); 
+            }
+                else if(grade >=50){
+                    console.log(`The grade is C`); 
+                }
+                    else if(grade >=40){
+                        console.log(`The grade is C-`); 
+                    }
+                        else if(grade >=30){
+                            console.log(`The grade is D`);
+                        } 
+
+    }
+    grading(30);
+    grading(60);
+    grading(88);
